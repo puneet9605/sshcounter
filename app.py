bfc795e4237ffa4a2e5d7e6699b510b29a426fd4
snpunee <snpunee@amazon.com>
2020-08-29 22:15:19 +0530

    Added sqllite DB and added the SSH model to the app

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/ssh_counter.db"
+db = SQLAlchemy(app)
+
+
+class ssh_count(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    count = db.Column(db.int)
+
 @app.route('/')
 def hello():
-    return "this is awesome"
+    return "ssh log-in attempts were made at "
