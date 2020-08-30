7e6c101e811b8d308ada4712d202e11d5223360f
snpunee <snpunee@amazon.com>
2020-08-30 23:25:17 +0530

    added route ssh counter correct the home page to show proper message

--- app.py
+++ app.py
@@ -2,15 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/ssh_counter.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tmp/ssh_counter.db"
 db = SQLAlchemy(app)
 
 
 class ssh_count(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    count = db.Column(db.int)
+    count = db.Column(db.Integer)
+
 
 @app.route('/')
 def hello():
-    return "ssh log-in attempts were made at "
+    text = ''
+    machines = ssh_count.query.all()
+    for machine in machines:
+        text += "{} ssh log-in attempts were made at {}<br>".format(machine.count, machine.name)
+    return text
+
+
+@app.route('/updatecounter/<string:machine_id>', methods=["POST"])
+def sshcounter(machine_id):
+    machine = ssh_count.query.filter_by(name=machine_id).all()
+    if not machine:
+        machine = ssh_count(name=machine_id, count=1)
+        db.session.add(machine)
+    else:
+        machine[0].count += 1
+    db.session.commit()
+    return "SSH attempt has been made for machine {}".format(machine_id)
